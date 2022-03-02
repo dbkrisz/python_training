614c936fc539fdf649dff16744570127c61ca8e4
dbkrisz <dbrisz@gmail.com>
2022-03-02 17:08:18 +0100

    input

---
+++ control_loop.py
@@ -0,0 +1,5 @@
+print ("begin")
+for i in range(0, 5): #fej
+    print("hello w") #törzs. behúzás fontos
+    print ("hello P")
+print ("exit")
---
+++ while_loop.py
@@ -0,0 +1,13 @@
+# Logikai = boolean, lehet true, false
+
+are_you_a_boy = True
+print (are_you_a_boy)
+print (type(are_you_a_boy))
+
+result = 10 < 20
+print (result)
+
+count=10
+while count !=0:
+    count = int(input ("adj egy számot"))
+    print (count *2)
